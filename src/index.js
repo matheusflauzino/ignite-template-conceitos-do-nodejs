7a313bd0347ce38229af649017b00bc10a812d66
Matheus Flauzino <matheusantonioflauzino@gmail.com>
2022-05-16 23:52:42 -0300

    feat: valida todo not found

--- src/index.js
+++ src/index.js
@@ -75,6 +75,10 @@ app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
   const { id } = request.params;
 
   const todo = user.todos.find(todo => todo.id === id);
+
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found!' });
+  }
   todo.title = title;
   todo.deadline = new Date(deadline);
   return response.json(todo);
@@ -85,12 +89,22 @@ app.patch('/todos/:id/done', checksExistsUserAccount, (request, response) => {
   const { id } = request.params;
 
   const todo = user.todos.find(todo => todo.id === id);
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found!' });
+  }
   todo.done = true;
   return response.json(todo);
 });
 
 app.delete('/todos/:id', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { user } = request;
+  const { id } = request.params;
+  const todo = user.todos.find(todo => todo.id === id);
+  if (!todo) {
+    return response.status(404).json({ error: 'Todo not found!' });
+  }
+  user.todos.splice(todo, 1);
+  return response.status(204).send();
 });
 
 module.exports = app;
